ae95495fc82bc634f08482dcde7a5bc66c61dda2
Prabal Pathak <prabal01pathak@gmail.com>
2021-06-12 21:06:14 +0530

    update mail sender

--- email_send.py
+++ email_send.py
@@ -4,28 +4,32 @@ from email.message import EmailMessage
 import imghdr
 from pathlib import Path
 
+
 EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
 contacts = [EMAIL_ADDRESS , 'prabal0100pathak']
 msg = EmailMessage()
-msg['Subject'] = 'Grab dinner this weekend?'
-msg['To']  = contacts
-msg['From'] = EMAIL_ADDRESS
-msg.set_content('How about dinner at 5pm this saturday?')
-path = Path('C:\\Users\\hp\\Downloads\\wp.jpg')
+msg['Subject'] = 'How to send mail'
+msg['To'] = contacts
+msg['From'] = EMAIL_ADDRESS 
+msg1 = EmailMessage()
+msg1['Subject'] = 'Grab dinner this weekend?'
+msg1['To']  = contacts
+msg1['From'] = EMAIL_ADDRESS
+msg1.set_content('How about dinner at 5pm this saturday?')
+path = Path('C:\\Users\\hp\\web\\files\\themes\\red\\new_resume.pdf')
 with open(path,'rb') as apple:
     file_data  = apple.read()
     file_type  = imghdr.what(apple.name)
     file_name  = path.stem
 
 
-msg.add_attachment(file_data,maintype = 'image',subtype = file_type ,filename = file_name)
 
 
 print('sending..')
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
     smtp.login(EMAIL_ADDRESS ,EMAIL_PASSWORD)
 
-    smtp.send_message(msg)
+    smtp.send_message(msg1)
     print('done')
 
